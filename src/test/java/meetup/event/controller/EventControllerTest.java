b7986ccc2ba73b5d9f71ce069bb86b4b05291b5b
Galina Lobacheva <147919859+KoshanSky1@users.noreply.github.com>
2024-11-19 23:44:45 +0300

    feat: add tests

--- src/test/java/meetup/event/controller/EventControllerTest.java
+++ src/test/java/meetup/event/controller/EventControllerTest.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
