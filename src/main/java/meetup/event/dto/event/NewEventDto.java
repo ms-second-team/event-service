c94ec8a8f1dbbd57699cc156af8cec454df7b743
vvbakhanovich <vv.bakh@yandex.ru>
2024-12-01 18:36:47 +0300

    feat: Add participant limit

--- src/main/java/meetup/event/dto/event/NewEventDto.java
+++ src/main/java/meetup/event/dto/event/NewEventDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -12,19 +13,27 @@ import static meetup.Constants.DATA_PATTERN;
 
 @Builder
 public record NewEventDto(
+
         @NotBlank(message = "Name can not be blank")
         String name,
+
         @NotBlank(message = "Description can not be blank")
         String description,
+
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATA_PATTERN)
         @Future(message = "Start date and time must be in future")
         @NotNull(message = "Start date and time cannot be null")
         LocalDateTime startDateTime,
+
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATA_PATTERN)
         @Future(message = "End date and time must be in future")
         @NotNull(message = "End date and time cannot be null")
         LocalDateTime endDateTime,
+
         @NotBlank(message = "Location cannot be null")
-        String location
+        String location,
+
+        @PositiveOrZero(message = "Participant limit must be positive or zero")
+        int participantLimit
 ) {
 }
--- src/main/java/meetup/event/model/event/Event.java
+++ src/main/java/meetup/event/model/event/Event.java
@@ -25,16 +25,27 @@ import java.time.LocalDateTime;
 @ToString
 @Builder
 public class Event {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
+
     @CreationTimestamp
     private LocalDateTime createdDateTime;
+
     private LocalDateTime startDateTime;
+
     private LocalDateTime endDateTime;
+
     private String location;
+
     @Column(name = "owner_id")
     private Long ownerId;
+
+    @Column(name = "participant_limit")
+    private int participantLimit;
 }
