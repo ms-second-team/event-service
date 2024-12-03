7348a7c7a10545cf3dfc9895e81e983ca4204954
kpetrosyan1 <karen-petrosyan1996@yandex.ru>
2024-12-03 21:21:39 +0300

    fix: add testcontainers config

--- src/test/java/meetup/EventServiceApplicationTest.java
+++ src/test/java/meetup/EventServiceApplicationTest.java
@@ -5,9 +5,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class EventServiceApplicationTest {
     @Autowired
     private EventController eventController;
